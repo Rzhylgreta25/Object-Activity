d9b526de578dc6f86e5e6481e425b4603294e9fc
Your Name <rzhyl05@gmail.com>
2024-10-20 19:17:37 +0800

    your commit message

---
+++ objectActivity.js
@@ -0,0 +1,13 @@
+let car = {
+    type: "Subaru",
+    model: "Civic",
+    color: "red"
+};
+
+console.log(typeof car);
+
+car.type = "Honda";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
